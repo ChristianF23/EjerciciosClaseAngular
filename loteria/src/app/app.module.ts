6060ed7f26cdbb719896c2ed066a9f06e9c81fcc
Christian Florez <55306489+ChristianF23@users.noreply.github.com>
2021-01-24 15:25:00 -0500

    Ejercicio loteria
    Comunicación entre componentes

---
+++ loteria/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BalotaComponent } from './balota/balota.component';
+import { ParImparComponent } from './par-impar/par-impar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BalotaComponent,
+    ParImparComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
